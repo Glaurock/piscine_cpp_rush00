ab10adddf34c8d9b9f3f561677094113a07a64bf
Glaurock <gregorymonnier1@gmail.com>
2019-01-12 12:15:05 +0100

    Ncurses logic moved into a class

--- srcs/Display.cpp
+++ srcs/Display.cpp
@@ -1,9 +1,25 @@
 #include "Display.hpp"
 
-Display::Display() {}
+Display::Display() {
+    initscr();
+    noecho();
+    cbreak();
+    start_color();
+    this->_win = newwin(ARENA_HEIGHT, ARENA_WIDTH, 0, 0);
+}
 
 Display::Display(Display const & src) {}
 
-Display::~Display() {}
+Display::~Display() {
+    // TODO: DESTROY THINGS FOR NCURSES?
+}
+
+void Display::draw(char *arena) {
+    curs_set(0);
+    wrefresh(this->_win);
+    mvwprintw(this->_win, 0, 0, arena);
+    wborder(this->_win, '|', '|', '-', '-', '+', '+', '+', '+');
+}
+
 
 Display & Display::operator=(Display const & src) {}
--- srcs/Display.hpp
+++ srcs/Display.hpp
@@ -2,18 +2,25 @@
 # define DISPLAY_HPP
 
 # include <ncurses.h>
+# include <cstring>
+# include <iostream>
+
+# define ARENA_WIDTH 100
+# define ARENA_HEIGHT 30
 
 class Display {
 
 private:
-    Display();
+    WINDOW * _win;
 
     Display(Display const & src);
     Display & operator=(Display const & src);
 
 public:
+    Display();
     virtual ~Display();
 
+    void draw(char *arena);    
 };
 
 # endif // DISPLAY_HPP
