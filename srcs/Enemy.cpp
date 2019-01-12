0cab7bb1148ed911ac3f1290efc48c69dc903fd2
Corentin SIMON <csimon@e2r2p23.42.fr>
2019-01-12 14:57:09 +0100

    Moving update

--- srcs/Enemy.cpp
+++ srcs/Enemy.cpp
@@ -6,9 +6,9 @@ Enemy::Enemy(int x, int y, int velocity, char type)
   static char list_type[5] = {'H', 'A', 'M', 'U', 'T'};
 
   random = 6;
-  // while (random > 5)
-    // random =
-    //     1 + std::rand() / ((RAND_MAX + 1u) / 5); // needs the ctime/iostream
+  while (random > 5)
+    random =
+        1 + std::rand() / ((RAND_MAX + 1u) / 5); // needs the ctime/iostream
   this->_type = list_type[random - 1];
   this->_velocity = (random % 2) + 1;
 }
@@ -33,4 +33,33 @@ void Enemy::move(void) {
     this->_velocity *= -1;  // edge was reached, reverse direction
   } else
     this->_x += velocity;
+  //   if (this->_y > ARENA_HEIGHT)
+  //     delete this;
+}
+
+void Enemy::move(char *arena) // safest method for BG check
+{
+  int prevX = this->getXCoordinate();
+  int prevY = this->getYCoordinate();
+  int velocity = this->getVelocity();
+
+  if ((prevX + velocity) > ARENA_WIDTH ||
+      (prevX + velocity) < 0) // a border of the window would be reached
+  {
+    if (velocity > 0)
+      this->_y += velocity;
+    else
+      this->_y -= velocity; // basic enemies only go in one general direction
+    this->_velocity *= -1;  // edge was reached, reverse direction
+  } else
+    this->_x += velocity;
+  if (arena[this->_x * this->_y] !=
+      ' ') // next case is, at the frame, not empty -> avoid crashing in
+           // backgrounds
+  {
+    this->_x = prevX;
+    this->_y = prevY;
+    this->_velocity *= -1;
+    return; // stay where you are
+  }
 }
--- srcs/Enemy.hpp
+++ srcs/Enemy.hpp
@@ -3,16 +3,18 @@
 
 #include "AGameEntity.hpp"
 
-class Enemy : public AGameEntity {
-private:
-  Enemy(Enemy const &src);
-  Enemy &operator=(Enemy const &src);
+class Enemy : public AGameEntity
+{
+  private:
+    Enemy(Enemy const &src);
+    Enemy &operator=(Enemy const &src);
 
-public:
-  Enemy();
-  Enemy(int x = 0, int y = 0, int velocity = 1, char type = 'H');
-  virtual ~Enemy();
-  void move(void); // can be overidden for more subtle patterns
+  public:
+    // Enemy();
+    Enemy(int x = 1, int y = 1, int velocity = 1, char type = 'H');
+    virtual ~Enemy();
+    void move(void); // can be overidden for more subtle patterns
+    void move(char *arena);
 };
 
 #endif // ENEMY_HPP
