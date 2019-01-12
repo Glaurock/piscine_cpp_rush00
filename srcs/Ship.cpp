21a67a41b4657bf212f826cf3cacda9adafdf81e
Glaurock <gregorymonnier1@gmail.com>
2019-01-12 13:35:41 +0100

    osef

--- srcs/Ship.cpp
+++ srcs/Ship.cpp
@@ -1,9 +1,20 @@
 #include "Ship.hpp"
 
-Ship::Ship() {}
+Ship::Ship(char* arena) {
+    this->_x = 10;
+    this->_y = 10;
+    this->_type = 'S';
+    this->_velocity = 2;
+    arena[this->_x * this->_y] = this->_type;
+}
 
-Ship::Ship(Ship const & src) {}
+// Ship::Ship(Ship const & src) {}
 
 Ship::~Ship() {}
 
-Ship & Ship::operator=(Ship const & src) {}
+bool Ship::move(char *arena) {
+
+}
+
+
+// Ship & Ship::operator=(Ship const & src) {}
--- srcs/Ship.hpp
+++ srcs/Ship.hpp
@@ -2,18 +2,20 @@
 # define SHIP_HPP
 
 # include "AGameEntity.hpp"
+# include "ft_retro.hpp"
 
 class Ship : public AGameEntity {
 
 private:
-
+    Ship();
     Ship(Ship const & src);
     Ship & operator=(Ship const & src);
 
 public:
-    Ship();
+    Ship(char *arena);    
     virtual ~Ship();
 
+    bool move(char *arena);
 };
 
 # endif // SHIP_HPP
