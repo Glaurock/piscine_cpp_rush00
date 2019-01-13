c1c2fa1298ce7158285c71e9a599452067651171
Corentin SIMON <csimon@e2r2p22.42.fr>
2019-01-13 10:27:34 +0100

    f

--- srcs/ft_retro.hpp
+++ srcs/ft_retro.hpp
@@ -1,14 +1,15 @@
 #ifndef FT_RETRO_HPP
 #define FT_RETRO_HPP
 
-#define ARENA_WIDTH 100
-#define ARENA_HEIGHT 30
+#define ARENA_WIDTH 150
+#define ARENA_HEIGHT 40
 
 #define MAX_ENEMIES 10
-#define MAX_MISSILES 4
+#define MAX_MISSILES 10
 #define MAX_BACKGROUNDS 400
+#define MAX_BACKGROUNDS_SIZE 7
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 #endif
